dbba25c7c3aa32bc5ae5948b63a3c4198e1b5639
dcraderdev <90993510+dcraderdev@users.noreply.github.com>
2022-12-03 13:50:19 -0800

    complete
    complete

--- parse-body.js
+++ parse-body.js
@@ -1,26 +1,52 @@
+
+"username=azure+green&password=password%21"
 function firstStep(input) {
-  // Your code here
+ return input.split("&")
 }
 
 function secondStep(input) {
-  // Your code here
+  let arr = []
+for(let i = 0; i < input.length; i++){
+  let t = input[i]
+  arr.push(t.split("="))
+
+}
+return arr
 }
 
 function thirdStep(input) {
-  // Your code here
+  const newPath = input.map(el => {
+    const ele = el.map(str => {
+      return str.replace('+', " ");
+    });
+    return ele;
+  });
+  return newPath;
 }
 
 function fourthStep(input) {
-  // Your code here
+  const newPath = input.map(el => {
+    const ele = el.map(str => {
+      return decodeURIComponent(str);
+    });
+    return ele;
+  });
+  return newPath;
 }
 
 function fifthStep(input) {
-  // Your code here
+  let obj = {}
+  for(let i = 0; i < input.length; i++){
+    let [a,b] = input[i]
+    obj[a]=b
+  }
+  return obj
 }
 
 function parseBody(str) {
-  // Your code here
+  return fifthStep(fourthStep(thirdStep(secondStep(firstStep(str)))))
 }
+
 
 /******************************************************************************/
 /******************* DO NOT CHANGE THE CODE BELOW THIS LINE *******************/
--- server.js
+++ server.js
@@ -5,7 +5,19 @@ let server;
 /******************************************************************************/
 /******************* DO NOT CHANGE THE CODE ABOVE THIS LINE *******************/
 
-// Your code here
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+
+
+ server = http.createServer((req,res) => {
+  
+ })
+
+
+
+
 
 /******************************************************************************/
 /******************* DO NOT CHANGE THE CODE BELOW THIS LINE *******************/
