e7b489c0062d6a299101c8ade33b796625543093
dcraderdev <90993510+dcraderdev@users.noreply.github.com>
2022-12-04 18:20:30 -0800

    Update server.js

--- server.js
+++ server.js
@@ -5,15 +5,38 @@ let server;
 /******************************************************************************/
 /******************* DO NOT CHANGE THE CODE ABOVE THIS LINE *******************/
 
-const http = require('http');
 const fs = require('fs');
 const path = require('path');
 
 
 
- server = http.createServer((req,res) => {
-  
- })
+const http = require('http');
+
+const port = 5005;
+
+server = http.createServer((req,res) => {
+  // if (req.method === 'GET' && req.url === '/') {
+  //   res.statusCode = 200;
+  //   res.setHeader('Content-Type', 'text/HTML');
+  //   return res.end('Server is listening on port 5000')
+
+
+
+    if (req.method === 'GET' && req.url === '/') {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/x-www-form-urlencoded');
+        return res.end('Server is listening on port 5000')
+    }
+    if (req.method === 'POST' && req.url === '/') {
+      res.statusCode = 302;
+      res.setHeader('Content-Type', 'text/plain');
+
+      return res.end('Server is listening on port 5000')
+  }
+
+
+
+})
 
 
 
